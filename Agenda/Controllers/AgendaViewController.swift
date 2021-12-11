2b2d5017236209272c70e0c26580ccfea7373b52
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-11 09:18:33 +0300

    refactor: remove 'getCurrentDate()' function

--- Agenda/Controllers/AgendaViewController.swift
+++ Agenda/Controllers/AgendaViewController.swift
@@ -13,21 +13,6 @@ class AgendaViewController: UITableViewController {
         super.viewDidLoad()
         
         view.backgroundColor = .white
-        getCurrentDate()
-    }
-    
-    func getCurrentDate() {
-        let date = Date()
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateStyle = .long
-        dateFormatter.timeStyle = .none
-        dateFormatter.locale = Locale(identifier: "en_US")
-        
-        let defaultString = dateFormatter.string(from: date)
-        let arrayOfElements = defaultString.split(separator: ",")
-
-        // navigationItem.title = "Agenda"
-        navigationItem.title = "\(dateFormatter.string(from: date))"
+        navigationItem.title = "Agenda"
     }
 }
