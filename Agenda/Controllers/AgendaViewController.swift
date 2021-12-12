d506f1dc5e6e7c198d959c2c0d5d9e253254767e
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-12 14:32:11 +0300

    fix: progressView of month is now displayed

--- Agenda/Controllers/AgendaViewController.swift
+++ Agenda/Controllers/AgendaViewController.swift
@@ -8,14 +8,19 @@
 import UIKit
 
 class AgendaViewController: UIViewController {
-
+    let dayAndMonth = UILabel()
+    let yearLabel = UILabel()
+    let progressView = UIProgressView()
+    // let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         navigationItem.title = "Agenda"
         
-        getMonthInfo()
+        getMonthInfo()  // view.addSubview(dateAndProgress)
+        // view.addSubview(tableView)
     }
     
     func getMonthInfo() {
@@ -26,37 +31,31 @@ class AgendaViewController: UIViewController {
         dateFormatter.locale = Locale(identifier: "en_US")
         
         let calendar = Calendar.current
-        // Calculate start and end of the current year (or month with `.month`):
-        let interval = calendar.dateInterval(of: .year, for: date)! //change year it will no of days in a year , change it to month it will give no of days in a current month
-        // Compute difference in days:
-        let days = calendar.dateComponents([.day], from: interval.start, to: interval.end).day!
-        
-        
-        let defaultString = dateFormatter.string(from: date)
-        let arrayOfElements = defaultString.split(separator: ",")
+        let days = calendar.range(of: .day, in: .month, for: date)!.count
+        let arrayOfElements = dateFormatter.string(from: date).split(separator: ",")
         
         // MARK: Date labels
-        let dayAndMonth = UILabel()
         dayAndMonth.attributedText = NSAttributedString(string: "\(arrayOfElements[0]),", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .bold)])
-        let year = UILabel()
-        year.attributedText = NSAttributedString(string: String(arrayOfElements[1]), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24)])
-        let dateView = UIStackView(arrangedSubviews: [dayAndMonth, year])
-        dateView.axis = .horizontal
+        yearLabel.attributedText = NSAttributedString(string: String(arrayOfElements[1]), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24)])
+        //yearLabel.addConstraint(yearLabel.leadingAnchor.constraint(equalTo: dayAndMonth.trailingAnchor, constant: 0)) // NSException
         
         // MARK: ProgressView
-        let progressView = UIProgressView()
-        progressView.progress = calendar.dat / days
-        // progressView.progress = 0.4
-        progressView.progressTintColor = UIColor(red: 69/255, green: 208/255, blue: 100/255, alpha: 1)
+        progressView.progress = Float(calendar.dateComponents([.day], from: date).day!) / Float(days)
+        // progressView.progressTintColor = UIColor(red: 69/255, green: 208/255, blue: 100/255, alpha: 1) // этот цвет используется для целей
+        
+        let dateView = UIStackView(arrangedSubviews: [dayAndMonth, yearLabel])
+        dateView.axis = .horizontal
+//        dateView.spacing = 0
+//        dateView.alignment = .leading
+//        dateView.distribution = .fillEqually
         
         let dateAndProgress = UIStackView(arrangedSubviews: [progressView, dateView])
         dateAndProgress.axis = .vertical
         view.addSubview(dateAndProgress)
+        
         dateAndProgress.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            dateAndProgress.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 2),
-            // titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 16),
-            // titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16)
+            dateAndProgress.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
             dateAndProgress.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             dateAndProgress.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
         ])
