edef3c7cc65405db73ca6ea2bb253654289e65dd
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-14 20:48:53 +0300

    fix: correct displaying yearLabel

--- Agenda/Controllers/AgendaViewController.swift
+++ Agenda/Controllers/AgendaViewController.swift
@@ -8,9 +8,21 @@
 import UIKit
 
 class AgendaViewController: UIViewController {
-    let dayAndMonth = UILabel()
-    let yearLabel = UILabel()
-    let progressView = UIProgressView()
+    var dayAndMonth: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var yearLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    var progressView: UIProgressView = {
+        let progress = UIProgressView()
+        progress.translatesAutoresizingMaskIntoConstraints = false
+        return progress
+    }()
     // let tableView = UITableView()
     
     override func viewDidLoad() {
@@ -19,7 +31,8 @@ class AgendaViewController: UIViewController {
         view.backgroundColor = .white
         navigationItem.title = "Agenda"
         
-        getMonthInfo()  // view.addSubview(dateAndProgress)
+        getMonthInfo()
+        setConstraints()  // view.addSubview(dateAndProgress)
         // view.addSubview(tableView)
     }
     
@@ -37,24 +50,25 @@ class AgendaViewController: UIViewController {
         dayAndMonth.attributedText = NSAttributedString(string: "\(arrayOfElements[0]),", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24, weight: .bold)])
         yearLabel.attributedText = NSAttributedString(string: String(arrayOfElements[1]), attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 24)])
         progressView.progress = Float(calendar.dateComponents([.day], from: date).day!) / Float(days)
-        
-        let dateView = UIStackView(arrangedSubviews: [dayAndMonth, yearLabel])
-        dateView.axis = .horizontal
-        dateView.setCustomSpacing(0, after: view)
-//        dateView.spacing = 0
-//        dateView.alignment = .leading
-//        dateView.isLayoutMarginsRelativeArrangement = false
-//        dateView.distribution = .fillProportionally
-        let dateAndProgress = UIStackView(arrangedSubviews: [progressView, dateView])
-        dateAndProgress.axis = .vertical
-        dateAndProgress.spacing = 2
-        view.addSubview(dateAndProgress)
-        
-        dateAndProgress.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func setConstraints() {
+        view.addSubview(progressView)
         NSLayoutConstraint.activate([
-            dateAndProgress.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
-            dateAndProgress.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            dateAndProgress.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -2),
+            progressView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            progressView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
+        
+        view.addSubview(dayAndMonth)
+        NSLayoutConstraint.activate([
+            dayAndMonth.topAnchor.constraint(equalTo: progressView.bottomAnchor, constant: 0),
+            dayAndMonth.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+        ])
+        view.addSubview(yearLabel)
+        NSLayoutConstraint.activate([
+            yearLabel.topAnchor.constraint(equalTo: progressView.bottomAnchor, constant: 0),
+            yearLabel.leadingAnchor.constraint(equalTo: dayAndMonth.trailingAnchor, constant: 0),
         ])
     }
 }
