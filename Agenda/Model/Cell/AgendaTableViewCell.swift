f89124bfe24f40edef62091fd68f3df4ce3f8540
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-15 09:14:31 +0300

    feat and refactor: add cell to the model and rename 'Model' group (directory)

---
+++ Agenda/Model/Cell/AgendaTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  AgendaTableViewCell.swift
+//  Agenda
+//
+//  Created by Егор Бадмаев on 15.12.2021.
+//
+
+import UIKit
+
+class AgendaTableViewController: UITableViewCell {
+    
+}
--- Agenda/Model/Goal.swift
+++ Agenda/Model/Goal.swift
@@ -12,7 +12,7 @@ struct Goal {
  }
 
  extension Goal {
-     static func getAims() -> [Goal] {
+     static func getGoals() -> [Goal] {
          return [
              Goal(title: "Дочитать книгу", current: 240, end: 450),
              Goal(title: "Прыжки со скакалкой", current: 90, end: 210),
