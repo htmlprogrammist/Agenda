ede4aace5fd015fc1ea3521e8a5dc2906fbe3e5c
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-18 21:45:15 +0300

    fix: displaying icons from iOS 14.0 in the 13th one

--- Agenda/Controllers/MainTabBarController.swift
+++ Agenda/Controllers/MainTabBarController.swift
@@ -16,18 +16,25 @@ class MainTabBarController: UITabBarController {
     }
     
     func createTabs() {
-        let agendaViewController = createNavController(viewController: AgendaViewController(), itemName: "Agenda", itemImage: "tray.full.fill")
-        let historyViewController = createNavController(viewController: HistoryViewController(), itemName: "History", itemImage: "clock.fill")
-        let summaryViewController = createNavController(viewController: SummaryViewController(), itemName: "Summary", itemImage: "newspaper.fill")
+        let agendaViewController = createNavController(viewController: AgendaViewController(), itemName: "Agenda", itemImage: "tray.full.fill", ios13CompabilityForIcon: false)
+        let historyViewController = createNavController(viewController: HistoryViewController(), itemName: "History", itemImage: "clock.fill", ios13CompabilityForIcon: false)
+        let summaryViewController = createNavController(viewController: SummaryViewController(), itemName: "Summary", itemImage: "square.text.square.fill", ios13CompabilityForIcon: true)
         
         viewControllers = [agendaViewController, historyViewController, summaryViewController]
     }
     
-    func createNavController(viewController: UIViewController, itemName: String, itemImage: String) -> UINavigationController {
-        let item = UITabBarItem(title: itemName, image: UIImage(systemName: itemImage), tag: 0)
+    func createNavController(viewController: UIViewController, itemName: String, itemImage: String, ios13CompabilityForIcon: Bool) -> UINavigationController {
+        
+        var imageNamed: UIImage?
+        switch ios13CompabilityForIcon {
+        case true:
+            imageNamed = UIImage(named: itemImage)
+        case false:
+            imageNamed = UIImage(systemName: itemImage)
+        }
         
         let navController = UINavigationController(rootViewController: viewController)
-        navController.tabBarItem = item
+        navController.tabBarItem = UITabBarItem(title: itemName, image: imageNamed, tag: 0)
         navController.navigationBar.prefersLargeTitles = true
         return navController
     }
