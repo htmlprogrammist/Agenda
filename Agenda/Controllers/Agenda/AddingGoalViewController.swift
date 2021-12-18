8008df6c3539bccb7dad8fceac2653814e4c5b4a
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-18 21:45:58 +0300

    feat: add bar button item for addingGoal view controller

--- Agenda/Controllers/Agenda/AddingGoalViewController.swift
+++ Agenda/Controllers/Agenda/AddingGoalViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class AddingGoalViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(addGoal))
+        // надо узнать, как сделать так, чтобы на эту кнопку нельзя было нажимать, пока все поля не будут заполнены
+        // соответственно, надо будет научиться подсвечивать каждое поле красным и писать ! required или типо того
+    }
+    
+    @objc func addGoal() {
+        
     }
 }
