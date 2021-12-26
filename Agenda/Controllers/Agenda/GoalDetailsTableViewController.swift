e21f330abe7db8563fd24fd74d17e0c3fa1630f0
Egor Badmaev <badmaeve2511@gmail.com>
2021-12-26 20:00:17 +0300

    fix: correct displaying text on each cell

--- Agenda/Controllers/Agenda/GoalDetailsTableViewController.swift
+++ Agenda/Controllers/Agenda/GoalDetailsTableViewController.swift
@@ -9,18 +9,40 @@ import UIKit
 
 class GoalDetailsTableViewController: UITableViewController {
     
+    var goal: Goal?
+    var idAgendaDetailsCell = "idAgendaDetailsCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
+        navigationItem.title = "Details"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = #colorLiteral(red: 0.9490196078, green: 0.9490196078, blue: 0.968627451, alpha: 1)
+        tableView.separatorStyle = .none
+        tableView.register(GoalDetailsTableViewCell.self, forCellReuseIdentifier: idAgendaDetailsCell)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         3
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return 3
+        case 2: return 1
+        default: return 1
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: <#T##String#>, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: idAgendaDetailsCell, for: indexPath)
+        cell.textLabel?.text = "Cell"
+        return cell
     }
 }
--- Agenda/Model/Cell/GoalDetailsTableViewCell.swift
+++ Agenda/Model/Cell/GoalDetailsTableViewCell.swift
@@ -8,5 +8,11 @@
 import UIKit
 
 class GoalDetailsTableViewCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
