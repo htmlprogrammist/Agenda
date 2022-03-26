26b31e67f2d04c654f84a113719c2e7f8f563dfd
Egor Badmaev <badmaeve2511@gmail.com>
2022-03-26 17:31:08 +0300

    feat: successfully created NSManagedObj subclasses

---
+++ Agenda/Models/CoreData/Month+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Month+CoreDataClass.swift
+//  Agenda
+//
+//  Created by Егор Бадмаев on 26.03.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Month: NSManagedObject {
+
+}
